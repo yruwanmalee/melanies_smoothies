c86c49d486a42b3989d66bc9064d87094cb895d2
yruwanmalee <yruwanmalee@gmail.com>
2025-12-01 21:20:24 -0600

    Update streamlit_app.py
    Importing pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 import requests
+import pandas as pd
 
 # Write directly to the app
 st.title("Customize Your Smoothie! :cup_with_straw: ")
@@ -14,7 +15,11 @@ st.write(
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe,use_container_width=True)
+#st.dataframe(data=my_dataframe,use_container_width=True)
+#st.stop()
+
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
 
 name_on_order = st.text_input("Name on Smoothie:")
